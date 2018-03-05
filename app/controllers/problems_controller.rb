47c58ce657d1f486054b5864b45f5644abc9675c
TJ <teddyjo@live.com>
2018-03-05 10:45:43 -0500

    Made :base_score a permitted parameter

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -62,6 +62,6 @@ private
 
   # this function says which problem attributes can be mass-assigned to, and which cannot
   def problem_params
-    params.require(:problem).permit(:name, :description, :max_score, :optional)
+    params.require(:problem).permit(:name, :description, :max_score, :optional, :base_score)
   end
 end
